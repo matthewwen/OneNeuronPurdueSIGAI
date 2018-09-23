b05b04ff200d8641ce2d6c669a60fb1f81795da0
Matthew Wen <mattwen2018@gmail.com>
2018-09-23 01:56:04 -0400

    The program compiles. Issue with the std:: infront of declaraion of vector

--- Runtime.cpp
+++ Runtime.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include "Housing.hpp"
 #include <vector>
+#include <math.h>
 
-double getAverage(vector<double> vec); 
+double getAverage(std::vector<double> vec); 
 
-int main(){
+int main()
+{
 
-	vector<double> x_values; //all the input values for a function
-	vector<double> y_values; //all the output values for a function 
+	std::vector<double> x_values(0); //all the input values for a function
+	std::vector<double> y_values(0); //all the output values for a function 
+
+	int x_average; //the average of all the input values
+	int y_average; //the average of all the output values
 
 	//example 
 	Housing h1(-117.66, 35.6,14,1740,391,850,317, 2.5812); 
@@ -16,8 +21,14 @@ int main(){
 	return 0; 
 }
 
-double getAverage(vector<double> vec){
-	if (vec.size == 0){
+
+/*
+* Parameter: vec, vector<double>, a vector with a set of values
+* Return: the average value
+*/
+double getAverage(std::vector<double> vec)
+{
+	if (vec.size() == 0){
 		return 0; 
 	}
 
