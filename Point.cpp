aea174012f33d427b437a1a7b91a3a0c79473021
Matthew Wen <mattwen2018@gmail.com>
2018-09-23 18:37:37 -0400

    Added comments for functions

--- Point.cpp
+++ Point.cpp
@@ -12,21 +12,37 @@ Point::Point(double x, double y)
     this->y = y; 
 }
 
+/*
+*Paramter: void, nothing  
+*Return: double, x value on coordinate plane 
+*/
 double Point::get_x()
 {
     return this->x; 
 }
 
+/*
+*Paramter: void, nothing  
+*Return: double, y value on coordinate plane 
+*/
 double Point::get_y()
 {
     return this->y; 
 }
 
+/*
+*Paramter: point, Point, point that have average input/output 
+*Return: double, least squre regression for y values
+*/
 double Point::get_least_squre_regression(Point point)
 {
     return 0.0; 
 } 
 
+/*
+*Paramter: point, Point, a point on coordinate plane 
+*Return: double, slope created from the two points 
+*/
 double Point::get_slope(Point point)
 {
     return 0.0; 
