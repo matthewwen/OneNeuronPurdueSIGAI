f3747f0426314fcb124d09efca34557c6d1bdfab
Matthew Wen <mattwen2018@gmail.com>
2018-09-23 13:20:38 -0400

    Added comments for methods

--- Housing.cpp
+++ Housing.cpp
@@ -22,46 +22,82 @@ Housing::Housing(double longitude, double latitude,
 	this->median_house_value = median_house_value; 
 }
 
+/*
+*Paramter: void, nothing 
+*Return: double, the longitude
+*/
 double Housing::get_longitude()
 {
 	return this->longitude; 
 }
 
+/*
+*Paramter: void, nothing 
+*Return: double, the latitude
+*/
 double Housing::get_latitude()
 {
 	return this->latitude; 
 } 
 
+/*
+*Paramter: void, nothing 
+*Return: double, the housing median age
+*/
 double Housing::get_housing_median_age()
 {
 	return this->housing_median_age; 
 } 
-		
+
+/*
+*Paramter: void, nothing 
+*Return: double, the total rooms
+*/		
 double Housing::get_total_rooms()
 {
 	return this->total_rooms; 
 }
 
+/*
+*Paramter: void, nothing 
+*Return: double, the total bedrooms 
+*/
 double Housing::get_total_bedrooms()
 {
 	return this->total_bedrooms; 
 }  
-		
+
+/*
+*Paramter: void, nothing 
+*Return: double, the population 
+*/		
 double Housing::get_population()
 {
 	return this->population; 
 }
-		
+
+/*
+*Paramter: void, nothing 
+*Return: double, the households 
+*/		
 double Housing::get_households()
 {
 	return this->households; 
 }
-		
+
+/*
+*Paramter: void, nothing 
+*Return: double, the median income 
+*/		
 double Housing::get_median_income()
 {
 	return this->median_income; 
 }
 
+/*
+* Paramter: index, int, the index of local variables (longitude, latitude, housing median age)
+* Return: Housing, the value (longitude, latitude, housing median)
+*/
 double Housing::get_value(int index)
 {
 	return get_switch_index_val(0, index) * this->longitude + 
@@ -75,6 +111,11 @@ double Housing::get_value(int index)
 	get_switch_index_val(8, index) * this->median_house_value; 
 }
 
+/*
+*Paramter: i, int, position of variable (longitude = 1). 
+		   index, int, index of the value the user want to use (longitude = 0, latitude = 1)
+*Return: double, the 
+*/
 int Housing::get_switch_index_val(int i, int index) 
 {
 	return ((i+1) % (index + 2) / (i + 1)) * ((i+1) % (index + 2) / (index + 1)); 
--- Runtime.cpp
+++ Runtime.cpp
@@ -21,11 +21,6 @@ int main()
 	//scan documents
 	scanHousing(&all_housing); 
 
-	//checking housing_median_age/ total_rooms/ median_house_value
-	double hma = all_housing[0].get_value(2); 
-	double tr = all_housing[0].get_value(3); 
-	double mhv = all_housing[0].get_value(8); 
-
 	//print the values saved in the first object
 	std::cout << "Housing Median Age: " << hma << std::endl; 
 	std::cout << "Total Rooms: " << tr << std::endl; 
@@ -34,40 +29,13 @@ int main()
 	//getting x values and w yavlues  
 	x_values = get_d_r_values(&all_housing, 0); 
 	y_values = get_d_r_values(&all_housing, 1); 
-	
-	//extra variables vectors 
-	std::vector<double> v01 = get_d_r_values(&all_housing, 2); 
-	std::vector<double> v02 = get_d_r_values(&all_housing, 3); 
-	std::vector<double> v03 = get_d_r_values(&all_housing, 4); 
-	std::vector<double> v04 = get_d_r_values(&all_housing, 5); 
-	std::vector<double> v05 = get_d_r_values(&all_housing, 6); 
-	std::vector<double> v06 = get_d_r_values(&all_housing, 7); 
-	std::vector<double> v07 = get_d_r_values(&all_housing, 8); 
 
 	///getting the average for domain and range 
 	x_average = getAverage(x_values); 
 	y_average = getAverage(y_values); 
 
-	//extra variables average
-	double d01 = getAverage(v01); 
-	double d02 = getAverage(v02); 
-	double d03 = getAverage(v03); 
-	double d04 = getAverage(v04); 
-	double d05 = getAverage(v05); 
-	double d06 = getAverage(v06); 
-	double d07 = getAverage(v07); 
-
-	std::cout << "0 average: " << x_average << std::endl; 
-	std::cout << "1 average: " << y_average << std::endl; 
-
-	//print result examples
-	std::cout << "2 average: " << d01 << std::endl; 
-	std::cout << "3 average: " << d02 << std::endl; 
-	std::cout << "4 average: " << d03 << std::endl; 
-	std::cout << "5 average: " << d04 << std::endl; 
-	std::cout << "6 average: " << d05 << std::endl; 
-	std::cout << "7 average: " << d06 << std::endl; 
-	std::cout << "8 average: " << d07 << std::endl; 
+	std::cout << "x average: " << x_average << std::endl; 
+	std::cout << "y average: " << y_average << std::endl; 
 
 
 	return 0; 
@@ -140,6 +108,12 @@ void scanHousing(std::vector<Housing> * vec)
 	ip.close(); 
 }
 
+
+/* 
+*Paramter: hous, address of Housing, list of all the housing options 
+*		   index, int, the logitutde etc. it want to return 
+*Return: vector<double> list of all the values for that particualar column (input/output)
+*/
 std::vector<double> get_d_r_values(std::vector<Housing> * hous, int index)
 {
 	std::vector<double> vec(0); 
