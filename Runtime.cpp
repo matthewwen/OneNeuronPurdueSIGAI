65c5b4f8b4ef6d7547a0606602a0a90de13e2477
Matthew Wen <mattwen2018@gmail.com>
2018-09-23 01:44:49 -0400

    Added Function in Runtime that calculates average of all the values inside of a vector

--- Runtime.cpp
+++ Runtime.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include "Housing.hpp"
+#include <vector>
+
+double getAverage(vector<double> vec); 
 
 int main(){
 
-	Housing h1(-117.66, 35.6,14,1740,391,850,317, 2.5812); 
+	vector<double> x_values; //all the input values for a function
+	vector<double> y_values; //all the output values for a function 
 
-	std::cout << "Housing 1 long: " << h1.get_longitude() << std::endl; 
+	//example 
+	Housing h1(-117.66, 35.6,14,1740,391,850,317, 2.5812); 
+ 
 
 	return 0; 
+}
+
+double getAverage(vector<double> vec){
+	if (vec.size == 0){
+		return 0; 
+	}
+
+	double sum = 0.0; 
+	for (int i = 0; i < vec.size(); i++){
+		sum += vec[i]; 
+	}
+
+	return sum / vec.size(); 
 }
